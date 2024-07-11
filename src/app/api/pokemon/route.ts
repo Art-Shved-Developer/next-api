078e042ed84e23de810be5b307a558b95583e67f
Artyom Bashkatov <ar.bashkatov@saychas.ru>
2024-07-11 12:22:57 +0300

    NextAPI ready

---
+++ src/app/api/pokemon/route.ts
@@ -0,0 +1,11 @@
+import { type NextRequest } from 'next/server'
+import { sql } from "@vercel/postgres";
+ 
+export async function GET(request: NextRequest) {
+  const searchParams = request.nextUrl.searchParams
+  const query = searchParams.get('name')
+
+  const pokemon = await sql`SELECT * FROM pokemons WHERE name = ${query};`;
+
+  return Response.json({ pokemon: pokemon.rows[0].data })
+}
