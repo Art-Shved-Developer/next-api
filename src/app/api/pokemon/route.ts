6485ca7bbec7f59dbb2a24793176c22ab34b9e35
Artyom Bashkatov <ar.bashkatov@saychas.ru>
2024-07-19 14:53:25 +0300

    update

--- src/app/api/pokemon/route.ts
+++ src/app/api/pokemon/route.ts
@@ -1,11 +1,12 @@
-import { type NextRequest } from 'next/server'
 import { sql } from "@vercel/postgres";
- 
+import { NextRequest, NextResponse } from "next/server";
+
 export async function GET(request: NextRequest) {
   const searchParams = request.nextUrl.searchParams
   const query = searchParams.get('name')
 
-  const pokemon = await sql`SELECT * FROM pokemons WHERE name = ${query};`;
+  const responseDb = await sql`SELECT * FROM pokemons WHERE name = ${query};`;
+  const result = responseDb.rows[0].data
 
-  return Response.json({ "pokemon": pokemon.rows[0].data})
+  return NextResponse.json({ "pokemon": JSON.stringify(result) }, { status: 200 })
 }
